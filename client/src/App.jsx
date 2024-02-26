a4e831eec8d5ee15b3c9ebb49633829099cf795f
Dilshan Rakshitha <dilshanr@boardpac.local>
2024-02-26 23:04:20 +0530

    Addig routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import DashBoard from './pages/DashBoard'
+import Projects from './pages/Projects'
+import Signin from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-600'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/dashboard' element={<DashBoard/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/signup' element={<SignUp/>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
