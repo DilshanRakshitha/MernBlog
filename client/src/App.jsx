de6616ecf719774819035752bd25a84998c026b7
Dilshan Rakshitha <dilshanr@boardpac.local>
2024-03-23 17:19:14 +0530

    Added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,19 @@ import DashBoard from './pages/DashBoard'
 import Projects from './pages/Projects'
 import Signin from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/dashboard' element={<DashBoard/>} />
         <Route path='/projects' element={<Projects/>} />
-        <Route path='/signin' element={<Signin/>} />
-        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/sign-in' element={<Signin/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
 
       </Routes>
     </BrowserRouter>
