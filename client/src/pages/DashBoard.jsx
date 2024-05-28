51e97228b39db438d87126969fdcfdf128cde1fa
Dilshan Rakshitha <dilshanrakshitha2000@gmail.com>
2024-05-28 15:54:28 +0530

    added theme

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function DashBoard() {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect()
   return (
     <div>DashBoard</div>
   )
